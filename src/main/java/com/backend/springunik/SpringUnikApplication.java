1a21c728be24687e356d11b389fb6b63c1c3359c
azamatshauenbekov <bravlstarstop05@mail.ru>
2024-11-14 08:35:09 +0500

    Initial commit

---
+++ src/main/java/com/backend/springunik/SpringUnikApplication.java
@@ -0,0 +1,13 @@
+package com.backend.springunik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUnikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/springunik/SpringUnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.springunik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUnikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
